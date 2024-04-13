0bb2e0f9ab1d6dfc8685bee361800de33baf15fd
Zerumi <afanasyev09032005@gmail.com>
2024-04-13 14:22:51 +0300

    feat: add ISA

---
+++ isa/src/main/kotlin/Opcode.kt
@@ -0,0 +1,18 @@
+enum class Opcode {
+    LOAD, // Load value to data stack
+    STORE, // Push value from data stack to memory
+    ADD, // Add 2 elements from stack, push result on stack
+    SUB, // Subtract 2 elements from stack, push result on stack
+    DROP, // Throw out value from stack
+    DUP, // Duplicate TOS on stack
+    OR, // Or 2 elements from stack, push result on stack
+    AND, // And 2 elements from stack, push result on stack
+    XOR, // Xor 2 elements from stack, push result on stack
+    JZ, // If TOS = 0, jump over one command
+    JUMP, // Set PC of memory address
+    CALL, // Call a subroutine
+    RET, // Return from a subroutine
+    IN, // Get buffer symbol from IO to stack
+    OUT, // Write symbol from stack to buffer
+    HALT // Terminate CPU
+}
