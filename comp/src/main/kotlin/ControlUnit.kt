4371b84703cbe9c3f27d0ad6d673afa3871bfdf0
Zerumi <afanasyev09032005@gmail.com>
2024-04-16 00:46:29 +0300

    feat: add microcode interpretation

--- comp/src/main/kotlin/ControlUnit.kt
+++ comp/src/main/kotlin/ControlUnit.kt
@@ -12,6 +12,7 @@ enum class Signal {
     JumpTypeJZ,
     JumpTypeJUMP,
     JumpTypeRET,
+    JumpTypeNext,
     MicroProgramCounterZero,
     MicroProgramCounterOpcode,
     MicroProgramCounterNext,
@@ -19,6 +20,8 @@ enum class Signal {
     TOSSelectMemory,
     TOSSelectDS,
     TOSSelectALU,
+    ARSelectTOS,
+    ARSelectPC,
 
     // I/O
     MemoryWrite, Output,
@@ -37,28 +40,45 @@ class ControlUnit(val initPc: Int, val program: Array<Instruction>) {
     val mProgram = arrayOf(
         /* Instruction fetch */
         /* 0 */ arrayOf(Signal.LatchMPCounter, Signal.MicroProgramCounterOpcode),
+        /* LIT */
+        /* 1 */ arrayOf(Signal.DataStackPush,
+                    Signal.LatchMPCounter, Signal.MicroProgramCounterNext),
+        /* 2 */ arrayOf(Signal.LatchTOS, Signal.TOSSelectMemory,
+                    Signal.LatchMPCounter, Signal.MicroProgramCounterNext),
         /* LOAD */
-        /* 1 */ arrayOf(Signal.DataStackPush, Signal.TOSSelectMemory, Signal.LatchTOS,
-                    Signal.LatchMPCounter, Signal.MicroProgramCounterZero),
+        /* 3 */ arrayOf(Signal.LatchAR, Signal.ARSelectTOS,
+                    Signal.LatchMPCounter, Signal.MicroProgramCounterNext),
+        /* 4 */ arrayOf(Signal.LatchTOS, Signal.TOSSelectMemory,
+                    Signal.LatchMPCounter, Signal.MicroProgramCounterZero,
+                    Signal.LatchPC, Signal.JumpTypeNext),
         /* STORE */
-        /* 2 */ arrayOf(Signal.LatchAR,
+        /* 5 */ arrayOf(Signal.LatchAR, Signal.ARSelectTOS,
                     Signal.LatchMPCounter, Signal.MicroProgramCounterNext),
-        /* 3 */ arrayOf(Signal.MemoryWrite, Signal.TOSSelectDS, Signal.LatchTOS,
+        /* 6 */ arrayOf(Signal.MemoryWrite,
                     Signal.LatchMPCounter, Signal.MicroProgramCounterNext),
-        /* 4 */ arrayOf(Signal.DataStackPop,
-                    Signal.LatchMPCounter, Signal.MicroProgramCounterZero),
+        /* 7 */ arrayOf(Signal.DataStackPop,
+                    Signal.LatchMPCounter, Signal.MicroProgramCounterNext),
+        /* 8 */ arrayOf(Signal.LatchTOS, Signal.TOSSelectDS,
+                    Signal.LatchMPCounter, Signal.MicroProgramCounterNext),
+        /* 9 */ arrayOf(Signal.DataStackPop,
+                    Signal.LatchMPCounter, Signal.MicroProgramCounterZero,
+                    Signal.LatchPC, Signal.JumpTypeNext),
         /* ADD */
-        /* 5 */ arrayOf(Signal.ALUSum, Signal.TOSSelectALU, Signal.LatchTOS,
+        /* 10 */ arrayOf(Signal.ALUSum, Signal.TOSSelectALU, Signal.LatchTOS,
                     Signal.LatchMPCounter, Signal.MicroProgramCounterNext),
-        /* 6 */ arrayOf(Signal.DataStackPop,
-                    Signal.LatchMPCounter, Signal.MicroProgramCounterZero),
+        /* 11 */ arrayOf(Signal.DataStackPop,
+                    Signal.LatchMPCounter, Signal.MicroProgramCounterZero,
+                    Signal.LatchPC, Signal.JumpTypeNext),
     )
 
     fun opcodeToMpc(opcode: Opcode): Int = when (opcode) {
-        Opcode.LOAD -> 1
-        Opcode.STORE -> 2
-        Opcode.ADD -> 5
+        Opcode.LIT -> 1
+        Opcode.LOAD -> 3
+        Opcode.STORE -> 5
+        Opcode.ADD -> 10
         Opcode.SUB -> TODO()
+        Opcode.INC -> TODO()
+        Opcode.DEC -> TODO()
         Opcode.DROP -> TODO()
         Opcode.DUP -> TODO()
         Opcode.OR -> TODO()
@@ -72,5 +92,39 @@ class ControlUnit(val initPc: Int, val program: Array<Instruction>) {
         Opcode.OUT -> TODO()
         Opcode.HALT -> TODO()
         else -> exitProcess(-1)
+    }
+
+    fun dispatchMicroInstruction(microcode: Array<Signal>) {
+        for (signal in microcode) {
+            when (signal) {
+                Signal.DataStackPush -> dataPath.onSignalDataStackPush()
+                Signal.DataStackPop -> dataPath.onSignalDataStackPop()
+                Signal.ReturnStackPush -> this.onSignalReturnStackPush()
+                Signal.ReturnStackPop -> this.onSignalReturnStackPop()
+                Signal.LatchAR -> dataPath.onSignalLatchAR(microcode)
+                Signal.LatchTOS -> dataPath.onSignalLatchTOS(microcode)
+                Signal.LatchPC -> this.onSignalLatchPC(microcode)
+                Signal.LatchMPCounter -> this.onSignalLatchMPCounter(microcode)
+                Signal.MemoryWrite -> dataPath.onSignalMemoryWrite()
+                Signal.Output -> dataPath.onSignalOutput()
+                else -> Unit
+            }
+        }
+    }
+
+    private fun onSignalLatchPC(microcode: Array<Signal>) {
+        TODO("Not yet implemented")
+    }
+
+    private fun onSignalReturnStackPop() {
+        TODO("Not yet implemented")
+    }
+
+    private fun onSignalReturnStackPush() {
+        TODO("Not yet implemented")
+    }
+
+    private fun onSignalLatchMPCounter(microcode: Array<Signal>) {
+        TODO("Not yet implemented")
     }
 }
--- comp/src/main/kotlin/DataPath.kt
+++ comp/src/main/kotlin/DataPath.kt
@@ -1,3 +1,25 @@
 class DataPath {
+    fun onSignalDataStackPush() {
+        TODO("Not yet implemented")
+    }
 
+    fun onSignalDataStackPop() {
+        TODO("Not yet implemented")
+    }
+
+    fun onSignalLatchAR(microcode: Array<Signal>) {
+        TODO("Not yet implemented")
+    }
+
+    fun onSignalLatchTOS(microcode: Array<Signal>) {
+        TODO("Not yet implemented")
+    }
+
+    fun onSignalOutput() {
+        TODO("Not yet implemented")
+    }
+
+    fun onSignalMemoryWrite() {
+        TODO("Not yet implemented")
+    }
 }
